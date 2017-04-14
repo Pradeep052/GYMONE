c458efefd51c385f48303fc1cf2bf6edd43b4e1b
ppv <ppv@ASI-INA532ZTP1>
2017-04-14 12:07:01 +0530

    sd

---
+++ GYM.DAL/Mappings/SchemeMasterMap.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GYM.DAL.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace GYM.DAL.Mappings
+{
+    public class SchemeMasterMap
+    {
+        public SchemeMasterMap(EntityTypeBuilder<SchemeMaster> entityBuilder)
+        {
+            entityBuilder.ToTable("SchemeMaster"); 
+            entityBuilder.HasKey(u => u.SchemeID);
+            entityBuilder.HasKey(u => u.SchemeName);
+            entityBuilder.HasKey(u => u.Createdby);
+            entityBuilder.HasKey(u => u.Createddate);            
+
+        }
+    }
+}
